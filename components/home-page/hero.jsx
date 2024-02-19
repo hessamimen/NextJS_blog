39fd46ac9fde83cf694729e1cd4922477cd4da6c
Hessam <hessam.imen@gmail.com>
2024-02-20 10:54:09 +1100

    feat: Implement the HomePage Hero section

--- components/home-page/hero.jsx
+++ components/home-page/hero.jsx
@@ -1,4 +1,22 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/hessam.jpeg"
+          alt="Hessam's photo"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Hessam</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like React
+        and Next Js
+      </p>
+    </section>
+  );
 }
 export default Hero;
