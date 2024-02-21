91ae01dd85724fdb452cf7a5cbb0ffb6793145f7
Hessam <hessam.imen@gmail.com>
2024-02-21 21:08:53 +1100

    feat: Implement the Contact form structure

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,6 @@
+import ContactForm from "../components/contact/contact-form";
+
 function ContactPage() {
-  return <div>ContactPage</div>;
+  return <ContactForm />;
 }
 export default ContactPage;
