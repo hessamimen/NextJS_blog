a248939902080b43d82946affd18b014e6272f6a
Hessam <hessam.imen@gmail.com>
2024-02-25 11:21:07 +1100

    feat: Store messages with MongoDB in a Database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,8 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+import "dotenv/config";
+
+const pass = process.env.MONGO_PASS;
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -10,14 +14,39 @@ function handler(req, res) {
       name.trim() === "" ||
       message.trim() === ""
     ) {
-      req.status(422).json({ message: "Invlide Input!" });
+      res.status(422).json({ message: "Invlide Input!" });
     }
     const newMessage = {
       email,
       name,
       message,
     };
-    console.log("newMessage:", newMessage);
+    // console.log("newMessage:", newMessage);
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://hessam:${pass}@cluster0.9dsbga4.mongodb.net/my-blog?retryWrites=true&w=majority&appName=Cluster0`
+      );
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!." });
+      return;
+    }
+
+    client.close();
+
     res
       .status(201)
       .json({ message: "Successfully stored message!", message: newMessage });
